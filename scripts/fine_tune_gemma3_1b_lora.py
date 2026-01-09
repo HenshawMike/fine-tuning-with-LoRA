e8c35591f053724a8e94b1335b0f7c2314e52c13
HenshawMike <mikelhenshaw11@gmail.com>
2026-01-09 06:09:30 +0100

    learning about models

--- scripts/fine_tune_gemma3_1b_lora.py
+++ scripts/fine_tune_gemma3_1b_lora.py
@@ -17,3 +17,28 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map= "cpu",
     low_cpu_mem_usage = True,
     )
+
+peft_config=LoraConfig(
+    r=16,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules= ["q_proj", "k_proj", "v_proj", "o_proj"],
+    )
+
+model = get_peft_model(model, peft_config)
+
+dataset = load_dataset("medalpaca/medical_meadow_medical_flashcards")
+dataset = dataset["train"].train_test_split(test_size=0.1)
+
+def format_example(example):
+    messages =[
+        {"role": "user", "content": example["question"]},
+        {"role": "assistant", "content": example["answer"]}
+    ]
+    return {"text": tokenize.apply_chat_template(messages, 
+            tokenize=False, 
+            add_generation_prompt=False)}
+
+dataset=dataset.map(format_example)
